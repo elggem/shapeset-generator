7a20649b13bcc066e47349f82f1808a22b360d2e
RalfMayet <trilly@gmail.com>
2017-03-07 10:22:22 +0100

    modified scaling/translation and added to config.

--- generate-samples.py
+++ generate-samples.py
@@ -31,17 +31,21 @@ for fn in os.listdir(CFG['input_folder']):
         rows,cols = img.shape
         
         rotate_angle = CFG['angle']/2 - int(CFG['angle'] * rnd.random())
-        scale_x = int((0.25 + (0.5 * rnd.random())) * rows)
-        scale_y = int((0.25 + (0.5 * rnd.random())) * cols)
+        scale_x = int((CFG['scale']/2 + (CFG['scale'] * rnd.random())) * rows)
+        scale_y = int((CFG['scale']/2 + (CFG['scale'] * rnd.random())) * cols)
         
-        translate_factor = 40
-        translate_x = scale_x / 2 + translate_factor/2 - int(translate_factor * rnd.random())
-        translate_y = scale_y / 2 + translate_factor/2 - int(translate_factor * rnd.random())
+        translate_factor = CFG['width']*CFG['translation']
+        translate_x = translate_factor/2 - int(translate_factor * rnd.random())
+        translate_y = translate_factor/2 - int(translate_factor * rnd.random())
                 
         M_translate = np.float32([[1,0,translate_x],[0,1,translate_y]])
         M_rotate = cv2.getRotationMatrix2D((cols/2,rows/2),rotate_angle,1)
 
         img = cv2.resize(img,(scale_x, scale_y), interpolation = cv2.INTER_CUBIC)
+        pad_x = (CFG['width']-img.shape[0])/2
+        pad_y = (CFG['height']-img.shape[1])/2
+        img = cv2.copyMakeBorder(img,pad_x,pad_x,pad_y,pad_y,cv2.BORDER_CONSTANT,value=[0,0,0])
+
         img = cv2.warpAffine(img, M_rotate, (cols,rows))
         img = cv2.warpAffine(img, M_translate, (cols,rows))
 
